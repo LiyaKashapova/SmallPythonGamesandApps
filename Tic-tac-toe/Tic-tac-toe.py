656f580c4a4b2d574bea23ef7f057d1d033a4b7c
Liya Kashapova <810.lili.lili@gmail.com>
2022-06-25 17:55:46 +0300

    Refactored Tic-Tac-Toe

--- Tic-tac-toe/Tic-tac-toe.py
+++ Tic-tac-toe/Tic-tac-toe.py
@@ -1,36 +1,4 @@
 from pygame import *
-import os
-
-
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
-
-
-lred = (255, 69, 0)
-red = (180, 0, 0)
-lblue = (0, 206, 209)
-blue = (30, 144, 255)
-black = (0, 0, 0)
-white = (245, 245, 245)
-
-files = {  # for easy .exe creation just wrap up in the function
-    'Saitama': resource_path('Saitama.png'),
-    'Genos': resource_path('Genos.png'),
-    'Bang': resource_path('Bang.png'),
-    'Sonic': resource_path('Sonic.png'),
-    'Fubuki': resource_path('Fubuki.png'),
-    'Garou': resource_path('Garou.png'),
-    'icon': resource_path('icon.png'),
-    'button': resource_path('button.png'),
-    'buttongrey': resource_path('buttongrey.png'),
-    'click': resource_path('click.mp3'),
-    'victory': resource_path('victory.mp3'),
-    'draw': resource_path('draw.ogg')
-}
 
 
 class GameSprite(sprite.Sprite):
@@ -46,24 +14,43 @@ class GameSprite(sprite.Sprite):
         self.color = c
 
     def draw(self):
-        draw.rect(w, self.color, self.rect, 3)
         if self.image:
             w.blit(self.image, (self.rect.x + self.sx, self.rect.y + self.sy))
+        draw.rect(w, self.color, self.rect, 3)
 
+
+lred = (255, 69, 0)
+red = (180, 0, 0)
+lblue = (0, 206, 209)
+blue = (30, 144, 255)
+black = (0, 0, 0)
+white = (245, 245, 245)
+
+mixer.init()
+files = {
+    'Saitama': image.load('Saitama.png'),
+    'Genos': image.load('Genos.png'),
+    'Bang': image.load('Bang.png'),
+    'Sonic': image.load('Sonic.png'),
+    'Fubuki': image.load('Fubuki.png'),
+    'Garou': image.load('Garou.png'),
+    'icon': image.load('icon.png'),
+    'button': image.load('button.png'),
+    'button_grey': image.load('buttongrey.png'),
+    'click': mixer.Sound('click.mp3'),
+    'victory': mixer.Sound('victory.mp3'),
+    'draw': mixer.Sound('draw.ogg')
+}
 
 sprites = {
-    'Saitama': GameSprite('Saitama', transform.scale(image.load(files['Saitama']), (100, 200)), black, 0, 0, 200, 200,
-                          50, 0),
-    'Genos': GameSprite('Genos', transform.scale(image.load(files['Genos']), (200, 200)), black, 0, 200, 200, 200, 0,
-                        0),
-    'Bang': GameSprite('Bang', transform.scale(image.load(files['Bang']), (200, 200)), black, 0, 400, 200, 200, 0, 0),
-    'Sonic': GameSprite('Sonic', transform.scale(image.load(files['Sonic']), (150, 200)), black, 400, 0, 200, 200, 20,
-                        0),
-    'Fubuki': GameSprite('Fubuki', transform.scale(image.load(files['Fubuki']), (100, 200)), black, 400, 200, 200, 200,
-                         50, 0),
-    'Garou': GameSprite('Garou', transform.scale(image.load(files['Garou']), (200, 200)), black, 400, 400, 200, 200, 0,
-                        0),
-    'default': GameSprite('rect', None, black, 200, 0, 200, 200, 0, 0)
+    'Saitama': GameSprite('Saitama', transform.scale(files['Saitama'], (100, 200)), black, 0, 0, 200, 200, 50, 0),
+    'Genos': GameSprite('Genos', transform.scale(files['Genos'], (200, 200)), black, 0, 200, 200, 200, 0, 0),
+    'Bang': GameSprite('Bang', transform.scale(files['Bang'], (200, 200)), black, 0, 400, 200, 200, 0, 0),
+    'Sonic': GameSprite('Sonic', transform.scale(files['Sonic'], (150, 200)), black, 400, 0, 200, 200, 20, 0),
+    'Fubuki': GameSprite('Fubuki', transform.scale(files['Fubuki'], (100, 200)), black, 400, 200, 200, 200, 50, 0),
+    'Garou': GameSprite('Garou', transform.scale(files['Garou'], (200, 200)), black, 400, 400, 200, 200, 0, 0),
+    'default': GameSprite('rect', None, black, 200, 0, 200, 200, 0, 0),
+    'button': GameSprite('button', files['button'], black, 225, 440, 150, 100, 0, 0)
 }
 
 field = [[sprites['Saitama'], sprites['Genos'], sprites['Bang']],
@@ -74,7 +61,7 @@ ww = 600
 wh = 600
 w = display.set_mode((ww, wh))
 display.set_caption('OPM Tic-Tac-Toe')
-display.set_icon(image.load(files['icon']))
+display.set_icon(files['icon'])
 w.fill(white)
 
 font.init()
@@ -82,38 +69,35 @@ f = font.SysFont('Impact', 35)
 w.blit(f.render('Choose', True, red), (240, 120))
 w.blit(f.render('Two', True, blue), (270, 170))
 w.blit(f.render('Characters', True, red), (220, 220))
-button = Rect(225, 440, 150, 100)
-draw.rect(w, black, button, 6)
-b = image.load(files['button'])
-bg = image.load(files['buttongrey'])
-w.blit(b, (225, 440))
 bn = f.render('Play', True, black)
+sprites['button'].draw()
 w.blit(bn, (270, 470))
-mixer.init()
-hit = mixer.Sound(files['click'])
-hit.set_volume(0.4)
-v = mixer.Sound(files['victory'])
-v.set_volume(0.4)
-d = mixer.Sound(files['draw'])
-d.set_volume(0.4)
+
+files['click'].set_volume(0.4)
+files['victory'].set_volume(0.4)
+files['draw'].set_volume(0.4)
 
 game = play = True
-players = ['l', 'r']  # [0] - sprite key (name)
+players = ['l', 'r']
 clock = time.Clock()
 
 
 def button_click():
-    hit.play()
-    global play, bn
-    w.blit(bg, (225, 440))
+    files['click'].play()
+    global play
+    sprites['button'].image = files['button_grey']
+    sprites['button'].draw()
     w.blit(bn, (270, 470))
     display.update()
     time.delay(200)
     if players[0] != 'l' and players[1] != 'r':
         play = False
     else:
-        w.blit(b, (225, 440))
+        sprites['button'].image = files['button']
+        sprites['button'].draw()
         w.blit(bn, (270, 470))
+        display.update()
+        time.delay(200)
 
 
 for fi in field:
@@ -125,10 +109,10 @@ while game and play:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if e.type == MOUSEBUTTONDOWN and e.button == 1:
+        if e.type == MOUSEBUTTONDOWN:
             x, y = e.pos
             if x < 200:
-                hit.play()
+                files['click'].play()
                 for fi in field[0]:
                     if fi.rect.collidepoint(x, y):
                         fi.color = lred
@@ -137,7 +121,7 @@ while game and play:
                         fi.color = black
                     fi.draw()
             elif x > 400:
-                hit.play()
+                files['click'].play()
                 for fi in field[2]:
                     if fi.rect.collidepoint(x, y):
                         fi.color = lblue
@@ -145,7 +129,7 @@ while game and play:
                     else:
                         fi.color = black
                     fi.draw()
-            if button.collidepoint(x, y):
+            if sprites['button'].rect.collidepoint(x, y):
                 button_click()
         clock.tick(60)
         display.update()
@@ -158,7 +142,7 @@ def draw_field():
         x = 0
         for j in range(3):
             name = field[i][j].name
-            field[i][j] = GameSprite(name, sprites[name].image, sprites[name].color, x, y, sprites[name].rect.width,
+            field[i][j] = GameSprite(name, files[name].image, sprites[name].color, x, y, sprites[name].rect.width,
                                      sprites[name].rect.height, sprites[name].sx, sprites[name].sy)
             field[i][j].draw()
             x += 200
@@ -194,7 +178,7 @@ while game and play:
         if e.type == QUIT:
             game = False
         if e.type == MOUSEBUTTONDOWN and e.button == 1:
-            hit.play()
+            files['click'].play()
             x, y = e.pos
             for i in range(3):
                 for j in range(3):
@@ -210,13 +194,15 @@ while game and play:
             if c == 'Draw':
                 w.fill(white)
                 w.blit(transform.scale(image.load(files[players[0]]),
-                       (sprites[players[0]].image.get_width() * 2, sprites[players[0]].image.get_height() * 2)),
+                                       (sprites[players[0]].image.get_width() * 2,
+                                        sprites[players[0]].image.get_height() * 2)),
                        (0 + sprites[players[0]].sx, 130 + sprites[players[0]].sy))
                 w.blit(transform.scale(image.load(files[players[1]]),
-                       (sprites[players[1]].image.get_width() * 2, sprites[players[1]].image.get_height() * 2)),
+                                       (sprites[players[1]].image.get_width() * 2,
+                                        sprites[players[1]].image.get_height() * 2)),
                        (300 + sprites[players[1]].sx, 130 + sprites[players[1]].sy))
                 w.blit(f.render('Draw!', True, black), (270, 50))
-                d.play()
+                files['draw'].play()
             elif c:
                 w.fill(white)
                 i = sprites[c].image
@@ -227,6 +213,6 @@ while game and play:
                     w.blit(transform.scale(image.load(files[c]), (i.get_width() * 2, i.get_height() * 2)),
                            (100 + sprites[c].sx, 130 + sprites[c].sy))
                 w.blit(f.render('Here\'s our winner!', True, sprites[c].color), (180, 50))
-                v.play()
+                files['victory'].play()
         clock.tick(60)
         display.update()
