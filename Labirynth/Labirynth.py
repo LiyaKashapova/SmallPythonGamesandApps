5a26a111b39dc5ed39d4cad13bbc7ea6e1df48a3
Liya Kashapova <810.lili.lili@gmail.com>
2022-08-21 18:29:15 +0300

    Refactored Labirynth

--- Labirynth/Labirynth.py
+++ Labirynth/Labirynth.py
@@ -1,18 +1,17 @@
 from pygame import *
-import pygame.time
 
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, x, y, s):
+    def __init__(self, i, x, y, s):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
-        self.speed = s
+        self.image = transform.scale(image.load(i), (65, 65))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.speed = s
 
-    def reset(self):
-        win.blit(self.image, (self.rect.x, self.rect.y))
+    def draw(self):
+        w.blit(self.image, (self.rect.x, self.rect.y))
 
 
 class Player(GameSprite):
@@ -20,109 +19,99 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_d] and self.rect.x < w - 80:
+        if keys[K_d] and self.rect.x < ww - 80:
             self.rect.x += self.speed
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < h - 80:
+        if keys[K_s] and self.rect.y < wh - 80:
             self.rect.y += self.speed
-        self.reset()
+        self.draw()
 
 
 class Enemy(GameSprite):
-    direction = "l"
+    direction = 'l'
 
     def update(self):
         if self.rect.x <= 470:
-            self.direction = "r"
-        if self.rect.x >= w - 85:
-            self.direction = "l"
-        if self.direction == "l":
+            self.direction = 'r'
+        elif self.rect.x >= ww - 85:
+            self.direction = 'l'
+        if self.direction == 'l':
             self.rect.x -= self.speed
         else:
             self.rect.x += self.speed
-        self.reset()
+        self.draw()
 
 
 class Wall(sprite.Sprite):
     def __init__(self, x, y, w, h):
         super().__init__()
-        self.w = w
-        self.h = h
-        self.s = Surface([self.w, self.h])
+        self.s = Surface([w, h])
         self.s.fill((154, 205, 50))
         self.rect = self.s.get_rect()
         self.rect.x = x
         self.rect.y = y
 
     def draw(self):
-        win.blit(self.s, (self.rect.x, self.rect.y))
+        w.blit(self.s, (self.rect.x, self.rect.y))
 
 
 def collide():
     if sprite.collide_rect(player, monster):
         return True
-    for w in walls:
-        if sprite.collide_rect(player, w):
+    for wall in walls:
+        if sprite.collide_rect(player, wall):
             return True
     return False
 
 
-w = 700
-h = 500
-win = display.set_mode((w, h))
-display.set_caption("Labirynth")
-background = transform.scale(image.load("back.jpg"), (w, h))
+init()
+ww, wh = 700, 500
+w = display.set_mode((ww, wh))
+display.set_caption('Labyrinth')
+display.set_icon(transform.scale(image.load('bananas.png'), (50, 50)))
+back = transform.scale(image.load('back.jpg'), (ww, wh))
 clock = time.Clock()
-player = Player('monkey.png', 5, h - 80, 4)
-monster = Enemy('tiger.png', w - 80, 280, 2)
-prize = GameSprite('bananas.png', w - 100, h - 80, 0)
-walls = [
-    Wall(100, 20, 450, 10),
-    Wall(100, 480, 350, 10),
-    Wall(100, 20, 10, 380),
-    Wall(200, 130, 10, 350),
-    Wall(450, 130, 10, 360),
-    Wall(300, 20, 10, 350),
-    Wall(390, 120, 130, 10)
-]
+player = Player('monkey.png', 5, wh - 80, 4)
+monster = Enemy('tiger.png', ww - 80, 280, 2)
+prize = GameSprite('bananas.png', ww - 100, wh - 80, 0)
+walls = [Wall(100, 20, 450, 10), Wall(100, 480, 350, 10), Wall(100, 20, 10, 380), Wall(200, 130, 10, 350),
+         Wall(450, 130, 10, 360), Wall(300, 20, 10, 350), Wall(390, 120, 130, 10)]
 font.init()
-font = font.SysFont('Impact', 70)
-won = font.render('YOU WON!', True, (255, 215, 0))
-lost = font.render('YOU LOST!', True, (180, 0, 0))
+f = font.SysFont('Impact', 70)
+won = f.render('You WON!', True, (255, 215, 0))
+lost = f.render('You LOST!', True, (180, 0, 0))
 mixer.init()
 mixer.music.load('jungles.mp3')
 mixer.music.play()
 yummy = mixer.Sound('drink.mp3')
 kick = mixer.Sound('kick.ogg')
 
-f = g = True
-while f:
+run = play = True
+while run:
     for e in event.get():
         if e.type == QUIT:
-            f = False
-    win.blit(background, (0, 0))
+            run = False
+    w.blit(back, (0, 0))
     for wall in walls:
         wall.draw()
-    prize.reset()
+    prize.draw()
     monster.update()
     player.update()
     if collide():
-        win.blit(lost, (200, 200))
+        w.blit(lost, (200, 200))
+        display.update()
         kick.play()
-        display.update()
-        clock.tick(60)
-        g = False
+        play = False
     if sprite.collide_rect(player, prize):
-        win.blit(won, (200, 200))
-        yummy.play()
+        w.blit(won, (200, 200))
         display.update()
-        clock.tick(60)
-        g = False
-    if not g:
+        yummy.play()
+        play = False
+    if not play:
         player.rect.x = 5
-        player.rect.y = h - 80
-        g = True
-        pygame.time.wait(300)
+        player.rect.y = wh - 80
+        play = True
+        time.wait(300)
     display.update()
     clock.tick(60)
