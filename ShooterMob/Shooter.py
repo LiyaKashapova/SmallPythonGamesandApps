596644aacf2a1082aab694a7cf93d8e9e8228db5
Liya Kashapova <49695972+LiyaKashapova@users.noreply.github.com>
2022-11-20 23:19:30 +0300

    Refactored Shooter: added images for enemies, added stroke for text, changed background - now it's pretty! Also simplified code, changed velocity of enemies, spheres (good&bad), player Finally, it looks good :-)

--- ShooterMob/Shooter.py
+++ ShooterMob/Shooter.py
@@ -1,5 +1,5 @@
 from pygame import *
-from random import randint
+from random import randint, uniform, choice
 from time import time as timer
 
 
@@ -26,7 +26,7 @@ class Player(GameSprite):
         self.draw()
 
     def fire(self):
-        bullet = Bullet(images['bullet'], self.rect.centerx, self.rect.top, 15)
+        bullet = Bullet(images['bullet'], self.rect.centerx, self.rect.top, 10)
         bullets.add(bullet)
 
 
@@ -51,14 +51,14 @@ class Bullet(GameSprite):
 
 images = {
     'icon': image.load('icon.png'),
-    'back': transform.scale(image.load('back.png'), (1000, 700)),
+    'back': transform.scale(image.load('back.jpg'), (1200, 700)),
     'button': image.load('button.png'),
     'buttonclicked': image.load('buttongrey.png'),
     'player': transform.scale(image.load('Shigeo.png'), (85, 170)),
     'bullet': transform.scale(image.load('lasers.png'), (30, 40)),
-    'enemy': transform.scale(image.load('ghost.png'), (200, 100)),
-    'bads': transform.scale(image.load('sphere.png'), (100, 100)),
-    'goods': transform.scale(image.load('blast.png'), (100, 100))
+    'enemies': [transform.scale(image.load(f'enemies/e{i}.png'), (100, 100)) for i in range(1, 16)],
+    'bads': transform.scale(image.load('sphere.png'), (50, 50)),
+    'goods': transform.scale(image.load('blast.png'), (50, 50))
 }
 
 init()
@@ -77,32 +77,40 @@ hit.set_volume(0.2)
 click.set_volume(0.2)
 
 font.init()
-f, color = font.SysFont('Impact', 80), (154, 38, 99)
-win, lose = f.render('YOU WON!', True, color), f.render('YOU LOST!', True, color)
+f, stroke, color = font.SysFont('Impact', 80), (154, 38, 99), (255, 204, 204)
+win_s, win = f.render('YOU WON!', True, stroke), f.render('YOU WON!', True, color)
+lose_s, lose = f.render('YOU LOST!', True, stroke), f.render('YOU LOST!', True, color)
 f = font.SysFont('Impact', 40)
 
 w.blit(images['back'], (0, 0))
+w.blit(f.render('Don''t let the monsters get through!', True, stroke), (198, 118))
 w.blit(f.render('Don''t let the monsters get through!', True, color), (200, 120))
-w.blit(f.render('Each                diminishes your allies''s morale', True, color), (150, 200))
-w.blit(images['bads'], (250, 200))
+w.blit(f.render('Each          diminishes your allies''s morale', True, stroke), (148, 198))
+w.blit(f.render('Each          diminishes your allies''s morale', True, color), (150, 200))
+w.blit(images['bads'], (235, 200))
+w.blit(f.render('as well as each monsters going through you', True, stroke), (148, 278))
 w.blit(f.render('as well as each monsters going through you', True, color), (150, 280))
-w.blit(f.render('Each                gives your allies hope, pick them up!', True, color), (120, 360))
-w.blit(images['goods'], (230, 360))
+w.blit(f.render('Each         gives your allies hope, pick them up!', True, stroke), (118, 358))
+w.blit(f.render('Each         gives your allies hope, pick them up!', True, color), (120, 360))
+w.blit(images['goods'], (205, 360))
 
 button = Rect(400, 490, 210, 110)
 draw.rect(w, (216, 191, 216), button, 5)
 w.blit(images['button'], (405, 495))
-play = f.render('Play', True, (51, 0, 102))
+play_s, play = f.render('Play', True, stroke), f.render('Play', True, color)
+w.blit(play_s, (468, 518))
 w.blit(play, (470, 520))
 
 
 def button_click():
     click.play()
     w.blit(images['buttonclicked'], (405, 495))
+    w.blit(play_s, (468, 518))
     w.blit(play, (470, 520))
     display.update()
     time.delay(200)
     w.blit(images['button'], (405, 495))
+    w.blit(play_s, (468, 518))
     w.blit(play, (470, 520))
     display.update()
     global game
@@ -124,10 +132,10 @@ def generate():
     global monsters, bullets, bads, goods
     monsters, bullets, bads, goods = sprite.Group(), sprite.Group(), sprite.Group(), sprite.Group()
     for i in range(1, 6):
-        monsters.add(Enemy(images['enemy'], randint(80, ww - 200), -40, randint(1, 3)))
+        monsters.add(Enemy(choice(images['enemies']), randint(80, ww - 200), randint(-60, -40), uniform(1, 2)))
     for i in range(1, 3):
-        bads.add(Enemy(images['bads'], randint(30, ww - 30), -40, randint(1, 5)))
-        goods.add(Enemy(images['goods'], randint(30, ww - 30), -40, randint(1, 5)))
+        bads.add(Enemy(images['bads'], randint(30, ww - 30), randint(-60, -40), uniform(1, 3)))
+        goods.add(Enemy(images['goods'], randint(30, ww - 30), randint(-60, -40), uniform(1, 3)))
 
 
 global monsters, bullets, bads, goods
@@ -136,8 +144,9 @@ generate()
 
 score = missed = fired = 0
 game, reload, ctime, rtime = True, False, None, None
+wait_s, wait = f.render('Wait, getting angry...', True, stroke), f.render('Wait, getting angry...', True, color)
+press_s, press = f.render('Press P to run again...', True, color), f.render('Press P to run again...', True, stroke)
 tcolor = color
-wait, press = f.render('Wait, getting angry...', True, color), f.render('Press P to run again...', True, (245, 245, 245))
 while run:
     for e in event.get():
         if e.type == QUIT:
@@ -168,7 +177,7 @@ while run:
         collides = sprite.groupcollide(monsters, bullets, True, True)
         for c in collides:
             score += 1
-            monsters.add(Enemy(images['enemy'], randint(80, ww - 200), -40, randint(1, 5)))
+            monsters.add(Enemy(choice(images['enemies']), randint(80, ww - 200), randint(-60, -40), uniform(1, 2)))
         for m in monsters:
             if m.rect.y > ww:
                 missed += 1
@@ -176,10 +185,10 @@ while run:
                 m.rect.y = -100
         if sprite.spritecollide(player, bads, True):
             missed += 1
-            bads.add(Enemy(images['bads'], randint(80, ww - 200), -40, randint(1, 5)))
+            bads.add(Enemy(images['bads'], randint(80, ww - 200), randint(-60, -40), uniform(1, 3)))
         if sprite.spritecollide(player, goods, True):
             missed -= 1
-            goods.add(Enemy(images['goods'], randint(80, ww - 200), -40, randint(1, 3)))
+            goods.add(Enemy(images['goods'], randint(80, ww - 200), randint(-60, -40), uniform(1, 3)))
         if missed < 3:
             tcolor = color
         elif 3 < missed < 5:
@@ -191,18 +200,25 @@ while run:
         bads.update()
         goods.update()
         bullets.update()
+        w.blit(f.render(f'Monsters: {50 - score}', True, stroke), (18, 18))
         w.blit(f.render(f'Monsters: {50 - score}', True, color), (20, 20))
         if missed > 10:
+            w.blit(f.render('Morale: 0', True, stroke), (18, 98))
             w.blit(f.render('Morale: 0', True, tcolor), (20, 100))
         else:
+            w.blit(f.render(f'Morale: {10 - missed}', True, stroke), (18, 98))
             w.blit(f.render(f'Morale: {10 - missed}', True, tcolor), (20, 100))
         if missed > 9:
             game = False
+            w.blit(lose_s, (348, 298))
             w.blit(lose, (350, 300))
+            w.blit(press_s, (318, 498))
             w.blit(press, (320, 500))
         elif score == 50:
             game = False
+            w.blit(win_s, (348, 298))
             w.blit(win, (350, 300))
+            w.blit(press_s, (318, 498))
             w.blit(press, (320, 500))
         display.update()
         clock.tick(60)
